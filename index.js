b89152e5856014be8e35d639cab4ddb2f75160ea
Steve Kinney <hello@stevekinney.net>
2016-08-23 05:13:22 -0600

    Add favicons

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http');
 const cors = require('express-cors');
 const bodyParser = require('body-parser');
+const hbs = require('hbs');
+
+http.Server(app);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Chat Box'
+app.locals.title = 'Chat Box';
 app.locals.messages = [];
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (request, response) => {
-  fs.readFile(`${__dirname}/public/index.html`, (err, file) => {
-    response.send(file);
-  });
+  response.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.get('/messages', (request, response) => {
@@ -24,8 +29,8 @@ app.get('/messages', (request, response) => {
 });
 
 app.get('/messages/:id', (request, response) => {
-  const { id } = request.params;
-  const message = app.locals.messages.find(m => m.id == id);
+  const id = parseInt(request.params.id, 10);
+  const message = app.locals.messages.find(m => m.id === id);
   if (message) { return response.send({ message }); }
   return response.sendStatus(404);
 });
@@ -48,8 +53,8 @@ app.post('/messages', (request, response) => {
 
 app.put('/messages/:id', (request, response) => {
   const { message } = request.body;
-  const { id } = request.params;
-  const index = app.locals.messages.findIndex((m) => m.id == id);
+  const id = parseInt(request.params.id, 10);
+  const index = app.locals.messages.findIndex((m) => m.id === id);
 
   if (index === -1) { return response.sendStatus(404); }
 
@@ -60,13 +65,13 @@ app.put('/messages/:id', (request, response) => {
 });
 
 app.delete('/messages/:id', (request, response) => {
-  const { id } = request.params;
-  if (!app.locals.messages.find((m) => m.id == id)) {
+  const id = parseInt(request.params.id, 10);
+  if (!app.locals.messages.find((m) => m.id === id)) {
     return response.status(404).send({
       error: 'There is no message with the "id" of ${id}.'
     });
   }
-  app.locals.messages = app.locals.messages.filter((m) => m.id != id);
+  app.locals.messages = app.locals.messages.filter((m) => m.id !== id);
   response.sendStatus(204);
 });
 
